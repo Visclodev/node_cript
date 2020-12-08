75d405261a4621928217e9b9def00fe3eea16b9b
schneider <Yobobis@gmail.com>
2020-12-08 19:38:05 +0100

    it's beautifull

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //a program to encript a message(process.argv[2])
 //with a key(process.argv[3])
+
 for (let i = 0, res = 0; i < process.argv[2].length; i++) {
   for (let j = 0; j < process.argv[3].length; j++) {
     res = res + process.argv[2].charCodeAt(i) * process.argv[3].charCodeAt(j);
