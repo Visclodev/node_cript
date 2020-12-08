0dab39c4b1a6a58dcd771be1b0bd2d2dd533ceb6
schneider <Yobobis@gmail.com>
2020-12-08 19:35:58 +0100

    removed useless var and added description

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-//process.argv 2 -> message
-//process.argv 3 -> key
-
-let message = "lolmdrptdr";
-let key = "patate";
-
-if (process.argc < 3 || process.argc > 3) {
-  console.error("not enough/too much arguments");
-}
-
+//a program to encript a message(process.argv[2])
+//with a key(process.argv[3])
 for (let i = 0, res = 0; i < process.argv[2].length; i++) {
   for (let j = 0; j < process.argv[3].length; j++) {
     res = res + process.argv[2].charCodeAt(i) * process.argv[3].charCodeAt(j);
