4f06ac1052cf18dcab606214533c641488930fa3
schneider <Yobobis@gmail.com>
2020-12-09 14:34:23 +0100

    encript and decript both working and output is in one line

--- decript.js
+++ decript.js
@@ -1,4 +1,19 @@
 //a program to decript a message(process.argv[(every argc > 2) - 1])
 //with a key(process.argv[last argv])
 
-for (let i = 0; i < process.argc)
+let res = 1;
+let x = 0
+
+for (let i = 2; i < process.argv.length - 1; i++) {
+    res = parseInt(process.argv[i])
+    while (res != 0) {
+        x++;
+        res = parseInt(process.argv[i]);
+        for (let j = process.argv[process.argv.length - 1].length - 1; j >= 0; j--) {
+            res = res - (process.argv[process.argv.length - 1].charCodeAt(j) * x);
+        }
+    }
+    process.stdout.write(String.fromCharCode(x));
+    x = 0;
+}
+console.log("");
--- encript.js
+++ encript.js
@@ -1,9 +1,10 @@
 //a program to encript a message(process.argv[2])
 //with a key(process.argv[3])
 
-for (let i = 0, res = 0; i < process.argv[2].length; i++) {
+for (let i = 0, res = 0; i < process.argv[2].length; i++, res = 0) {
   for (let j = 0; j < process.argv[3].length; j++) {
     res = res + process.argv[2].charCodeAt(i) * process.argv[3].charCodeAt(j);
   }
-  console.log(res);
+  process.stdout.write(res + " ");
 }
+console.log("");
